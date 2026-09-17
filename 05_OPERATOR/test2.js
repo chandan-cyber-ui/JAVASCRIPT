f2cc01c66eac9c3d6713d01855b924cdb8003804
Chandan Swain <chandan@example.com>
2026-09-17 19:42:49 +0530

    first commit

---
+++ 05_OPERATOR/test2.js
@@ -0,0 +1,2 @@
+let signalColor="red";
+console.log(signalColor=="red"?"stop":signalColor=="green"?"go":signalColor=="yelloe"?"ready to go":"invalid")
---
+++ 05_OPERATOR/test3.js
@@ -0,0 +1,2 @@
+let mark= +prompt("enetr your no")
+console.log(mark)
